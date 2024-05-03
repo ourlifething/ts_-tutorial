920c7d7c87b46354f42f44710ed643faccb48d46
ourlifething <ryuji.hayama@gmail.com>
2024-05-03 14:22:10 +0900

    number.boolean型推論

---
+++ basics.ts
@@ -0,0 +1,14 @@
+export {}
+console.log('ここにコードを追加!')
+function add(n1: number, n2: number, showResult: boolean) {
+    // if (typeof n1 !== 'number' || typeof n2 !== 'number') {
+    //     throw new Error('入力値が正しくありません')
+    // }
+    return n1 + n2
+}
+const number1 = 5
+const number2 = 2.8;
+const printResurt = true;
+
+const result = add(number1, number2, printResurt);
+console.log(result);
